c1c3a830f60806bfdb1fa40a978271671a1ea23d
korekorokero <koresy.samuel20@gmail.com>
2025-03-25 08:25:47 +0700

    added create, read, and delete

---
+++ lib/pages/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/model/note_model.dart';
+import 'package:notesapp/pages/add_note.dart';
+import 'package:notesapp/pages/note_card.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            padding: const EdgeInsets.only(
+              top: 12.0,
+              left: 12.0,
+              right: 12.0,
+              bottom: 80.0
+            ),
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return NoteCard(
+                title: notes[index].title,
+                content: notes[index].content,
+                onDelete: () async {
+                  setState(() {
+                    _isLoading = true;
+                  });
+
+                  await Future.delayed(const Duration(seconds: 1));
+
+                  setState(() {
+                    notes.removeAt(index);
+                    _isLoading = false;
+                  });
+                },
+              );
+            },
+          ),
+          if (_isLoading)
+          Container(
+            color: Colors.black54,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newNote = await showAddNoteBottomSheet(context);
+          if (newNote != null) {
+            setState(() {
+              _isLoading = true;
+            });
+
+            await Future.delayed(const Duration(seconds: 1));
+
+            setState(() {
+              notes.add(NoteModel(
+                title: newNote[0],
+                content: newNote[1],
+              ));
+              _isLoading = false;
+            });
+          }
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/pages/note_card.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class NoteCard extends StatelessWidget {
+  final String title;
+  final String content;
+  final VoidCallback onDelete;
+
+  const NoteCard({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.onDelete
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    content,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 12.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                InkWell(
+                  borderRadius: BorderRadius.circular(8.0),
+                  onTap: () {
+                    print('Edit note');
+                  },
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      color: Colors.amber.shade200,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Icon(Icons.edit, size: 20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                InkWell(
+                  borderRadius: BorderRadius.circular(8.0),
+                  onTap: onDelete,
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      color: Colors.red.shade200,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Icon(Icons.delete, size: 20.0),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
